a12d84c247afdc96c9dbc94efdae16186feb0310
hunteryavitz <hunteryavitz@protonmail.com>
2023-09-04 12:59:34 -0700

    added submit transaction with status

--- src/components/SubmitTransactionForm.tsx
+++ src/components/SubmitTransactionForm.tsx
@@ -1,29 +1,64 @@
-import React, { useState } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import React, { useState } from 'react'
+import {View, Button, StyleSheet, Text, Alert} from 'react-native'
+import { Picker } from '@react-native-picker/picker'
+import HTTP from '../api/http-commons'
 
 const SubmitTransactionForm = () => {
+    const STATUS_VALUES = [
+        'CREATED',
+        'PENDING',
+        'FULFILLED',
+        'REJECTED',
+        'CANCELLED',
+        'SHIPPED',
+        'DELIVERED',
+        'RETURNED',
+        'REFUNDED',
+        'RECEIVED',
+        'COMPLETED',
+    ]
 
-    const [selectedValue, setSelectedValue] = useState("CREATED");
+    const [selectedValue, setSelectedValue] = useState(STATUS_VALUES[0])
 
-    const handleSubmit = () => {
-        alert('Your transaction has been submitted!');
+    const transaction = {
+        id: '-1',
+        timestamp: 'never',
+        source: 'nowhere',
+        status: 'REJECTED'
+    }
+
+    const handleSubmit = async () => {
+        const updatedTransaction = {
+            ...transaction,
+            id: new Date().getSeconds() + new Date().getMilliseconds(),
+            timestamp: new Date().toISOString(),
+            source: 'mobile',
+            status: selectedValue,
+        }
+
+        try {
+            const response =
+                await HTTP.post('/transaction/submitTransaction', updatedTransaction);
+            console.log(response)
+            Alert.alert('Submit Transaction - Succeed')
+        } catch (error) {
+            console.log(error)
+            Alert.alert('Submit Transaction - Failed')
+        }
     }
 
     return (
         <View style={styles.container}>
+            <Text>Submit Transaction</Text>
             <Picker
                 selectedValue={selectedValue}
                 style={styles.picker}
-                onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+                onValueChange={(itemValue) => setSelectedValue(itemValue)}
             >
-                <Picker.Item label="Created" value="CREATED" />
-                <Picker.Item label="Pending" value="PENDING" />
-                <Picker.Item label="Completed" value="COMPLETED" />
-                <Picker.Item label="Cancelled" value="CANCELLED" />
+                {STATUS_VALUES.map(status => <Picker.Item key={status} label={status} value={status} />)}
             </Picker>
             <Button
-                title="Submit"
+                title="Submit Transaction"
                 onPress={handleSubmit}
             />
         </View>
